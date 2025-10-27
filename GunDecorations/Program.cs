72a6ce46cea39b1b293c2716fe235d72558f468f
txger. <simaocampanico.dev@gmail.com>
2025-10-27 14:21:21 +0000

    GunDecorations project

---
+++ GunDecorations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GunDecorations
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
